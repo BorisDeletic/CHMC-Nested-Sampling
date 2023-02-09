098ac5a7c05594313ea1ef9d4a08c61c83b62000
boris <borisdeletic0@gmail.com>
2023-02-09 18:56:31 +0000

    first working version of CMake with enzyme. Succesfully compiles main.cpp. Next add it as a dependency

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ double dsquare(double x) {
     // This returns the derivative of square or 2 * x
     return __enzyme_autodiff((void*) square, x);
 }
+
 int main() {
     for(double i=1; i<5; i++)
         printf("square(%f)=%f, dsquare(%f)=%f", i, square(i), i, dsquare(i));
