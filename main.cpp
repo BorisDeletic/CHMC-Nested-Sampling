09270de4d7d4f700db888afdb75772dc77aac6f9
borisdeletic <borisdeletic0@gmail.com>
2023-02-09 13:23:47 +0000

    defined folder structure, adding gtest module. working to add enzyme as library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
-
+// test.c
+#include <stdio.h>
+extern double __enzyme_autodiff(void*, double);
+double square(double x) {
+    return x * x;
+}
+double dsquare(double x) {
+    // This returns the derivative of square or 2 * x
+    return __enzyme_autodiff((void*) square, x);
+}
 int main() {
-    std::cout << "Hello, World! first commit" << std::endl;
-    return 0;
+    for(double i=1; i<5; i++)
+        printf("square(%f)=%f, dsquare(%f)=%f", i, square(i), i, dsquare(i));
 }
