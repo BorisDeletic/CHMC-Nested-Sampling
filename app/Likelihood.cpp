f72626e749f43db9b866c38cf156f8406cbb8119
borisdeletic <borisdeletic0@gmail.com>
2023-02-11 23:27:39 +0000

    need to decide how to do pointer to member functions for autodiff

--- app/Likelihood.cpp
+++ app/Likelihood.cpp
@@ -2,28 +2,22 @@
 // Created by Boris Deletic on 11/02/2023.
 //
 
-#include "ILikelihood.h"
+#include "Likelihood.h"
 
 int enzyme_dup;
 int enzyme_const;
 extern double __enzyme_autodiff(...);
 
-class Likelihood : public ILikelihood {
-    double likelihood(double *theta, int n) override {
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += theta[i] * theta[i] * theta[i];
-        }
-        return sum;
+double Likelihood::likelihood(double *theta, int n) {
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += theta[i] * theta[i] * theta[i];
     }
+    return sum;
+}
 
-    double gradient(double* theta, double* d_theta, int size) {
-        double (ILikelihood::*func)(double*, int);
-        func = &ILikelihood::likelihood;
-        // This returns the derivative of square or 2 * x
-        return __enzyme_autodiff(func,
-                                 enzyme_dup, theta, d_theta,
-                                 enzyme_const, size);
-    }
-
-};
+double Likelihood::gradient(double* theta, double* d_theta, int size) {
+    return __enzyme_autodiff(likelihood,
+                             enzyme_dup, theta, d_theta,
+                             enzyme_const, size);
+}
